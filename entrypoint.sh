8b6386399a60f24571b8e5b5315fa6c0336f8a9f
Dorian Koch <dorian.koch@rwth-aachen.de>
2026-05-14 17:22:05 +0200

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,5 @@ set -e
 cp /app/crontab /etc/cron.d/bbc-noticias
 chmod 0644 /etc/cron.d/bbc-noticias
 
-# Validate crontab syntax before starting cron
-if ! cron -T -n 2>/dev/null; then
-    echo "ERROR: crontab validation failed" >&2
-    exit 1
-fi
-
 # Start cron daemon
 exec cron -f
